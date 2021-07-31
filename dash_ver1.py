78c4f1590f56b10f377bcd1817927c740e261e88
Laura <lauraamydaines@gmail.com>
2021-07-31 09:02:08 +0200

    Starting feature selection version

---
+++ dash_ver1.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+
+st.title("Credit Home Dashboard:")
+st.sidebar.title("Share Price analysis for May 2019 to May 2020:")
+st.markdown("This application is a Share Price dashboard for Top 5 Gainers and Losers:")
+st.sidebar.markdown("This application is a Share Price dashboard for Top 5 Gainers and Losers:")
+
+streamlit run share_new.py
